7bb487d673e4d4e6ca02aa676113b5b247180f86
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-10-25 00:13:33 +0200

    fix: Fix slide page not exported

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { default as Image } from "./components/image/image";
 export { default as LucideIcon } from "./components/lucide-icon/lucide_icon";
 export { default as Markdown } from "./components/markdown/markdown";
 export { default as Modal } from "./components/modal/modal";
-export { default as Slideshow } from "./components/slideshow/slideshow";
+export { default as Slideshow, SlidePage } from "./components/slideshow/slideshow";
 export { useIdleMouse, useIdleMouseProvider, type IdleMouseProviderConfig } from "./hooks/use_idle_mouse";
 export { useDebouncer } from "./utils/useDebouncer";
