20cc56d77b4f2d72dff3b3be0bbfbf1f32ab16b3
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-10-26 00:11:39 +0200

    feat: Allow disabling default styles in markdown

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -4,12 +4,13 @@ import styles from "./markdown.scss?inline";
 
 export default component$<{
   text: string,
+  disable_default_styles?: boolean,
 }>(props => {
   useStylesScoped$(styles);
   const formatted = useComputed$(() => marked.parse(props.text));
 
   return <div
-    class="markdown"
+    class={["markdown", { "default-styles": !(props.disable_default_styles ?? false) }]}
     dangerouslySetInnerHTML={formatted.value}
   />;
 });
